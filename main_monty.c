765f0bc76788a4434e77bd9d533b845300eb2ff0
somaftees <somaftees@gmail.com>
2023-10-18 16:18:43 -0700

    done

--- main_monty.c
+++ main_monty.c
@@ -1,5 +1,7 @@
 #include "monty.h"
 
+globales_t globalvar = {NULL, NULL, NULL};
+
 /**
  * main - entry point.
  * @argc: count of argument.
@@ -9,7 +11,6 @@
 
 int main(int argc, char **argv)
 {
-	globales_t globalvar = {NULL, NULL, NULL};
 	char *token_type = NULL;
 	size_t buf_line_s = 0;
 	int line_number = 0, flag = 0, flag2 = 0;
@@ -36,7 +37,8 @@ int main(int argc, char **argv)
 		if (flag2 == 0)
 		{
 			if (token_type[0] == '#')
-			{size_of_l = getline(&globalvar.line_buf,
+			{
+				size_of_l = getline(&globalvar.line_buf,
 						    &buf_line_s, globalvar.fd);
 				flag = 1; }}
 		if (flag == 0)
