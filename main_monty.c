8e65a4eed6e912921d0baccda0697cc96a565463
somaftees <somaftees@gmail.com>
2023-10-18 16:20:48 -0700

    done

--- main_monty.c
+++ main_monty.c
@@ -1,7 +1,5 @@
 #include "monty.h"
 
-globales_t globalvar = {NULL, NULL, NULL};
-
 /**
  * main - entry point.
  * @argc: count of argument.
@@ -11,6 +9,7 @@ globales_t globalvar = {NULL, NULL, NULL};
 
 int main(int argc, char **argv)
 {
+	globales_t globalvar = {NULL, NULL, NULL};
 	char *token_type = NULL;
 	size_t buf_line_s = 0;
 	int line_number = 0, flag = 0, flag2 = 0;
